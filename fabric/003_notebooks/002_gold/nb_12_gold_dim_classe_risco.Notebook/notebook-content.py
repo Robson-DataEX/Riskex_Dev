b46497efc77e234cfc8f1e99b3d5bc11c01d3bc2
Robson-DataEX <robson.mazzarotto@dataex.com.br>
2026-06-22 09:56:19 -0300

    Committing 9 items from workspace b248fd3e-e82e-4081-b9c3-84074fb5659a

---
+++ fabric/003_notebooks/002_gold/nb_12_gold_dim_classe_risco.Notebook/notebook-content.py
@@ -0,0 +1,179 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse_name": "",
+# META       "default_lakehouse_workspace_id": ""
+# META     }
+# META   }
+# META }
+
+# MARKDOWN ********************
+
+# 
+# # Tabela: riscos.dim_classe_risco
+# ## Objetivo:
+# Esta tabela os possíveis resultados de um risco ocupacional
+# 
+# -------------
+# 
+# #### Histórico de alterações
+# | Data | Desenvolvido por | Modificações |
+# |---|---|---|
+# | 18/06/2026 | Robson Mazzarotto| Criação do notebook |
+
+# MARKDOWN ********************
+
+# ## Configurações Iniciais
+
+# CELL ********************
+
+# MAGIC %%configure -f
+# MAGIC {
+# MAGIC     "defaultLakehouse": {
+# MAGIC         "name": "lh_gold",
+# MAGIC         "workspaceId":{
+# MAGIC             "parameterName":"workspace_id",
+# MAGIC             "defaultValue":""
+# MAGIC         }
+# MAGIC     }
+# MAGIC }
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+context = notebookutils.runtime.context
+workspace = context['currentWorkspaceName']
+defaultLakehouseName = context['defaultLakehouseName']
+workspace_id = context['currentWorkspaceId']
+
+print(workspace)
+print(workspace_id)
+print(defaultLakehouseName)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+%run nb_functions
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# PARAMETERS CELL ********************
+
+write_table_model = ""
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# Target Delta Table Variables
+ 
+container = 'lh_gold'
+target_schema = 'riscos'
+table_name = 'dim_lasse_risco'
+
+delta_table_name = f"{workspace}.{container}.{target_schema}.{table_name}"
+ 
+delta_file = f"abfss://{workspace}@onelake.dfs.fabric.microsoft.com/{container}.Lakehouse/Tables/{target_schema}/{table_name}"
+ 
+merge_columns = "Id"
+partition_columns = 'N/A'
+table_type = 'managed' #managed or external
+
+write_table_model = write_table_model ## "overwrite" ou "append" ou "merge"
+
+
+print(f"Tabela Delta: {delta_table_name}")
+print(f"Path Delta: {delta_file}")
+print(f"Colunas de Merge: {merge_columns}")
+print(f"Tipo de Tabela: {table_type}")
+print(f"Tipo de escrita da tabela: {write_table_model}")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# ### Preparação dos dados
+
+# CELL ********************
+
+source_dataframe = spark.sql(f"""
+
+SELECT 
+    Id,
+    Modulo,
+    PSR,
+    Inicio,
+    Fim,
+    Peso,
+    CorFundo,
+    CorTexto,
+    ClasseAceitavel,
+    SYNC_DATE,
+    ClasseRiscoIdioma_Descricao,
+    ClasseRiscoIdioma_Criterio,
+    insert_date,
+    update_date,
+    hash_row
+FROM {workspace}.lh_silver.riscos.classe_risco
+
+""")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# ### Carga e atualização dos dados
+
+# CELL ********************
+
+if source_dataframe.count() > 0:
+    automated_load(source_dataframe, delta_file, table_name, delta_table_name, write_table_model, merge_columns, table_type, partition_columns)
+
+else:
+    notebookutils.notebook.exit('O dataframe de origem está vazio e não há dados para carga/atualização.')
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
